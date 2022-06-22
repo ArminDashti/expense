93adea22f10e9e119ee77dd51eed0f0f74373c79
Armin <arminonline71@gmail.com>
2022-06-22 16:33:57 +0430

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4a0ksu17)a^@@4&s81mks+un6wt%wq()lgbx5!#l+azbuu%x3@'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['185.110.189.124','localhost']
+ALLOWED_HOSTS = ['***']
 
 
 # Application definition
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {'default': {'ENGINE':'django.db.backends.postgresql_psycopg2', 'NAME':'postgres', 'USER':'postgres', 'PASSWORD':'why789@@@trryhf_hi_no', 'HOST':'localhost', 'PORT':'5432'}} 
+DATABASES = {'default': {'ENGINE':'django.db.backends.postgresql_psycopg2', 'NAME':'***', 'USER':'***', 'PASSWORD':'***', 'HOST':'***', 'PORT':'***'}} 
 
 
 # Password validation
